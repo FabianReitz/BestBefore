a49be744ecb8a0f526e82e9c4d01078b2bb34a3f
FabianReitz <reitz@stadtwerk.org>
2021-05-04 16:38:27 +0200

    [💾] Added connection to database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import secrets from './secrets/secrets.js';
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = `mongodb+srv://${secrets.mongoDB.login}:${secrets.mongoDB.password}@cluster0.anro0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const PORT = 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
