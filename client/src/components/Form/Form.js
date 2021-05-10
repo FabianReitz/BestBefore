0d5df80ffd73dacfc7a2d080565204533b3eabba
FabianReitz <reitz@stadtwerk.org>
2021-05-10 11:47:53 +0200

    [⌨️] Removed unused variable, added react-router-dom

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, Button, Typography, Paper, Checkbox, FormControlLabel } from '@material-ui/core';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 
