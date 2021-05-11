5b728811d487487d6b0c32c72412e763ece9e7e0
FabianReitz <reitz@stadtwerk.org>
2021-05-11 14:20:35 +0200

    Working on Date

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import { MuiPickersUtilsProvider, KeyboardTimePicker, DatePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 import { useDispatch } from 'react-redux';
+import moment from 'moment';
 
 
 import useStyles from './styles';
@@ -11,7 +14,7 @@ const Form = () => {
     const [itemData, setItemData] = useState({
         title: '',
         manufacturer: '',
-        purchaseDate: '',
+        purchaseDate: new Date(),
         bestBeforeDate: '',
         ammount: '',
         isPackaged: false,
@@ -31,6 +34,10 @@ const Form = () => {
 
     }
 
+    const correctDateFormat = (input) => {
+        return moment(input, "DD.MM.YYYY", true).isValid();
+    }
+
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
@@ -40,9 +47,35 @@ const Form = () => {
 
                 <TextField name="manufacturer" variant="outlined" label="Hersteller" fullWidth value={itemData.manufacturer} onChange={(e) => setItemData({ ...itemData, manufacturer: e.target.value })} />
 
-                <TextField name="purchaseDate" variant="outlined" label="Kaufdatum" fullWidth value={itemData.purchaseDate} onChange={(e) => setItemData({ ...itemData, purchaseDate: e.target.value })} />
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
 
-                <TextField name="bestBeforeDate" variant="outlined" label="Mindesthaltbarkeitsdatum" fullWidth value={itemData.bestBeforeDate} onChange={(e) => setItemData({ ...itemData, bestBeforeDate: e.target.value })} />
+                    <DatePicker
+                        variant="inline"
+                        inputVariant="outlined"
+                        format="MM/dd/yyyy"
+                        margin="normal"
+                        autoOk
+                        id="date-picker-inline"
+                        label="Date picker inline"
+                        value={itemData.purchaseDate}
+                        onChange={(e) => setItemData({ ...itemData, purchaseDate: e.target.value })}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
+                    />
+
+                </MuiPickersUtilsProvider>
+
+
+                <TextField errorText="wow" name="purchaseDate" variant="outlined" label="Kaufdatum" fullWidth value={itemData.purchaseDate} onChange={(e) => {
+                    setItemData({ ...itemData, purchaseDate: e.target.value });
+                }} />
+
+
+                <TextField type="Number" name="bestBeforeDate" variant="outlined" label="Mindesthaltbarkeitsdatum" fullWidth value={itemData.bestBeforeDate} onChange={(e) => {
+                    setItemData({ ...itemData, bestBeforeDate: e.target.value });
+                    correctDateFormat(e.target.value);
+                }} />
 
                 <TextField name="ammount" variant="outlined" label="Menge" fullWidth value={itemData.ammount} onChange={(e) => setItemData({ ...itemData, ammount: e.target.value })} />
 
