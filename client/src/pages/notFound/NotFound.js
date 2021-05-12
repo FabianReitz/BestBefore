1304c7505b5b4131dd00bdeb04aa9665b0129982
FabianReitz <reitz@stadtwerk.org>
2021-05-12 18:09:18 +0200

    [🚫]Added a basic not-found stub for later testing

--- client/src/pages/notFound/NotFound.js
+++ client/src/pages/notFound/NotFound.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
+import {
+    AppBar,
+    Toolbar,
+    CssBaseline,
+    IconButton,
+    Typography,
+    Button,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import useStyles from './styles';
+
 const NotFound = () => {
-    return <h2>Not Found :/</h2>;
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position='static'>
+                <Toolbar>
+                    <IconButton
+                        edge='start'
+                        className={classes.menuButton}
+                        color='inherit'
+                        aria-label='menu'
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant='h6' className={classes.title}>
+                        BestBefore
+                    </Typography>
+                    <Button>Login</Button>
+                </Toolbar>
+            </AppBar>
+            <h1>Error 404</h1>
+            <small>This is not the page you are looking for</small>
+        </div>
+    );
 };
 
 export default NotFound;
