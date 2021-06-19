5d6e2c9b3f988e7eadc299fb7b50fb1784def5bb
FabianReitz <reitz@stadtwerk.org>
2021-06-19 13:31:41 +0200

    [🗑] Server delete route

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -27,3 +27,18 @@ export const createItem = async (req, res) => {
         res.status(409).json({ message: error.message });
     }
 };
+
+export const deleteItem = async (req, res) => {
+    const itemID = req.body.itemID;
+
+    try {
+        if (!itemID) return res.send(404);
+        console.log(itemID);
+        ItemDescription.deleteOne({ _id: itemID }, function (error) {
+            if (error) res.status(500).json({ message: error.message });
+        });
+        res.sendStatus(200);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
